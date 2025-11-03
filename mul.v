b5a813fb9d308f2a1250aa4f1615f50d8ca77b01
RahulAK122005 <146237608+RahulAK122005@users.noreply.github.com>
2025-11-03 19:32:01 +0530

    Update mul.v

--- mul.v
+++ mul.v
@@ -1,8 +1,7 @@
 module mul (
-    input  [3:0] A, B,
-    output [7:0] product
+    input  [3:0] A, B,      // inputs A and B
+    output [7:0] product    // output product
 );
-    assign product = A * B;
+    assign product = A * B;   // multiply A and B
 
 endmodule
-
