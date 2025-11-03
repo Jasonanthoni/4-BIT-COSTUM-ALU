c168d7d0c5c359440d64507522c69cb4efbd5019
JASON ANTHONI <105900282+Jasonanthoni@users.noreply.github.com>
2025-11-03 16:05:40 +0530

    Update avg.v

--- avg.v
+++ avg.v
@@ -1,8 +1,9 @@
-module avg(input [3:0] A, input [3:0] B, output [7:0] Y);
-    wire [7:0] sum;
-    wire [7:0] avg;
-    ADD add1 (.A(A), .B(B), .Y(sum));  // instantiate the addition module
-    assign avg = sum >> 1;  // right shift by 1 = divide by 2  
-    assign Y = avg;
+module avg (
+    input  [3:0] A, 
+    input  [3:0] B, 
+    output [7:0] Y
+);
+    wire [4:0] full_sum = {1'b0, A} + {1'b0, B}; 
+    wire [4:0] average_5bit = full_sum >> 1;
+    assign Y = {3'b000, average_5bit}; 
 endmodule
-
