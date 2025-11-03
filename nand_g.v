2cb595c64b5bb80f1d33146e21ff05f371658ec4
RahulAK122005 <146237608+RahulAK122005@users.noreply.github.com>
2025-11-03 19:33:21 +0530

    Update nand_g.v

--- nand_g.v
+++ nand_g.v
@@ -1,8 +1,6 @@
 module nand_g(
-    input [3:0] A, B,
-    output [7:0] Y
+    input [3:0] A, B,     // inputs A and B
+    output [7:0] Y        // output Y
 );
-    assign Y = {4'b0000, ~(A & B)};
+    assign Y = {4'b0000, ~(A & B)};   // perform bitwise NAND and extend to 8 bits
 endmodule
-
-
