d800f2555c8544b764fa022096fe71591899c62a
RahulAK122005 <146237608+RahulAK122005@users.noreply.github.com>
2025-11-03 19:35:51 +0530

    Update sub.v

--- sub.v
+++ sub.v
@@ -1,10 +1,15 @@
 module sub (
-    input  [3:0] A, B,
-    output [3:0] DIFF,
-    output COUT // Borrow out
+    input  [3:0] A, B,      // inputs A and B
+    output [3:0] DIFF,      // output difference
+    output COUT             // output borrow flag
 );
-    wire [4:0] temp;
-    assign temp = {1'b0, A} - {1'b0, B};  
-    assign DIFF = temp[3:0];              
-    assign COUT = ~temp[4];               
+
+    wire [4:0] temp;        // 5-bit wire to hold result with borrow
+
+    assign temp = {1'b0, A} - {1'b0, B};  // perform subtraction
+
+    assign DIFF = temp[3:0];      // assign lower 4 bits as difference          
+
+    assign COUT = ~temp[4];          // invert MSB to get borrow out         
+    
 endmodule
