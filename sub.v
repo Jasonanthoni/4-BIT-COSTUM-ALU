6a7f0bf2f0f13761a759b06bd264eb980d33da33
JASON ANTHONI <105900282+Jasonanthoni@users.noreply.github.com>
2025-11-03 15:49:34 +0530

    Update sub.v

--- sub.v
+++ sub.v
@@ -1,8 +1,12 @@
 module sub (
     input  [3:0] A, B,
     output [3:0] DIFF,
-    output BORROW
+    output BORROW_OUT 
 );
-    assign {BORROW, DIFF} = A-B;
-
+    
+    wire [3:0] B_inv = ~B;
+    wire C_out; 
+    assign {C_out, DIFF} = A + B_inv + 1'b1;
+    assign BORROW_OUT = ~C_out; 
+    
 endmodule
